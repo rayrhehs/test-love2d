86e8bc63ce6d1d6d2ba900c932c98d435d8998a2
Shehryar <shehryarm1029@gmail.com>
2025-12-16 01:36:11 -0500

    basic movement

--- main.lua
+++ main.lua
@@ -1,3 +1,25 @@
-function love.draw() 
-    love.graphics.print("Hello World!", 400, 300)
+function love.load()
+    player = {}
+    player.x = 400
+    player.y = 200
+    player.speed = 3
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("right") then
+        player.x = player.x + 3
+    end
+    if love.keyboard.isDown("left") then
+        player.x = player.x - 3
+    end
+    if love.keyboard.isDown("up") then
+        player.y = player.y - 3
+    end
+    if love.keyboard.isDown("down") then
+        player.y = player.y + 3
+    end
+end
+
+function love.draw()
+    love.graphics.circle("fill", player.x, player.y, 50)
 end
