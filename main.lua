246e53ad35323f5ccc4c6b8324f3b37236e047ea
Shehryar <shehryarm1029@gmail.com>
2025-12-22 16:10:29 -0500

    added basic physics to player

--- main.lua
+++ main.lua
@@ -1,25 +1,44 @@
+local debug = true
+
 function love.load()
     player = {}
-    player.x = 400
+    player.x = 370
     player.y = 200
     player.speed = 3
+    player.velocity = 5
+end
+
+function DrawPlayer()
+    love.graphics.rectangle("fill", player.x, player.y, 50, 50)
+end
+
+-- function 
+function love.keypressed(key)
+    if key == "f3" then
+        debug = not debug
+    end
+
+    if key == "space" then
+        -- load functionality
+        player.y = player.y - 120
+    end
+
 end
 
 function love.update(dt)
-    if love.keyboard.isDown("right") then
-        player.x = player.x + 3
-    end
-    if love.keyboard.isDown("left") then
-        player.x = player.x - 3
-    end
-    if love.keyboard.isDown("up") then
-        player.y = player.y - 3
-    end
-    if love.keyboard.isDown("down") then
-        player.y = player.y + 3
-    end
+    player.y = player.y + player.velocity
 end
 
+-- primary function that draws everything
 function love.draw()
-    love.graphics.circle("fill", player.x, player.y, 50)
+    DrawPlayer()
+
+    if debug then
+        love.graphics.print(
+            "FPS: " .. love.timer.getFPS() ..
+            "\nPlayer X: " .. player.x ..
+            "\nPlayer Y: " .. player.y,
+            10, 10
+        )
+    end
 end
