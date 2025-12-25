56c01126f8ccb8371f6161ef4c67099924027ea6
Shehryar <shehryarm1029@gmail.com>
2025-12-25 12:59:04 -0500

    added collisions with pipes and score

--- main.lua
+++ main.lua
@@ -2,13 +2,15 @@ local debug = true
 
 function love.load()
     player = {}
+    player.sprite = love.graphics.newImage('sprites/bird.png')
     player.x = 170
     player.y = 200
+    player.width = player.sprite:getWidth()
+    player.height = player.sprite:getHeight()
     player.speed = 3
     player.velocity = 0
     player.gravity = 500
     player.jumpStrength = -300
-    player.sprite = love.graphics.newImage('sprites/bird.png')
 
     background = {}
     background.x = 0
@@ -33,6 +35,11 @@ function love.load()
     pipes.sprite = {}
     pipes.sprite.pipe_up = love.graphics.newImage('sprites/pipe_up.png')
     pipes.sprite.pipe_down = love.graphics.newImage('sprites/pipe_down.png')
+
+    game = {}
+    game.state = true
+    game.score = 0
+    game.pointPassed = false
 end
 
 function DrawPlayer()
@@ -64,7 +71,15 @@ function DrawPipe()
     if pipes.x <= -pipes.width then
         pipes.x = 400
         pipes.y = love.math.random(64, 350)
+        game.pointPassed = false
     end
+end
+
+function checkCollision(x1, y1, w1, h1, x2, y2, w2, h2)
+    return x1 < x2 + w2 and
+           x2 < x1 + w1 and
+           y1 < y2 + h2 and
+           y2 < y1 + h1
 end
 
 
@@ -87,10 +102,25 @@ function love.update(dt) -- dt = ~0.0167
     background.x = background.x - 80 * dt
     ground.x = ground.x - 200 * dt
     pipes.x = pipes.x - 150 * dt
+
+    -- increase score b/c this is checking for entire pipes obj (top and bottom)
+    if player.x >= pipes.x and not game.pointPassed then
+        game.score = game.score + 1
+        game.pointPassed = true
+    end
+
+    if checkCollision(player.x, player.y, player.width, player.height, pipes.x, pipes.y - pipes.height, pipes.width, pipes.height) or checkCollision(player.x, player.y, player.width, player.height, pipes.x, pipes.y + pipes.gap, pipes.width, pipes.height) then
+        player.velocity = 0
+        game.state = false
+    end
 end
 
 -- primary function that draws everything
 function love.draw()
+    if not game.state then
+        return
+    end
+
     DrawBackground()
     DrawPipe()
     DrawGround()
@@ -100,7 +130,9 @@ function love.draw()
         love.graphics.print(
             "FPS: " .. love.timer.getFPS() ..
             "\nPlayer X: " .. player.x ..
-            "\nPlayer Y: " .. player.y,
+            "\nPlayer Y: " .. player.y ..
+            "\nScore: " .. game.score ..
+            "\nGame Over: " .. (game.state and "false" or "true"),
             10, 10
         )
     end
